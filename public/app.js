e1a07423e45fca95543820a3251ab6c1c8c7d907
Lane Anderson <laneanderson92@gmail.com>
2018-01-12 15:59:01 -0600

    Added stuff

--- public/app.js
+++ public/app.js
@@ -1,36 +1,37 @@
-// Click Events
-
-// Click event to add a book to the db
-$("#addbook").on("click", function() {
+$(document).on("click", ".boxcast-btn", function() {
+  console.log('Youtube Streaming false');
   $.ajax({
     type: "POST",
-    url: "/submit",
+    url: "/update",
     dataType: "json",
     data: {
-      title: $("#title").val(),
-      author: $("#author").val(),
-      created: Date.now()
+      status: false
     }
   })
   .done(function(data) {
     console.log(data);
-    getUnread();
-    $("#author").val("");
-    $("#title").val("");
-  }
-  );
+    getStatus();
+  });
   return false;
 });
 
 // Click event to mark a book as read
-$(document).on("click", ".markread", function() {
-  var thisId = $(this).attr("data-id");
+$(document).on("click", ".youtube-btn", function() {
+  console.log('Youtube Streaming True');
   $.ajax({
-    type: "GET",
-    url: "/markread/" + thisId
+    type: "POST",
+    url: "/update",
+    dataType: "json",
+    data: {
+      status: true,
+      name: 'Lane Anderson'
+    }
+  })
+  .done(function(data) {
+    console.log(data);
+    getStatus();
   });
-  $(this).parents("tr").remove();
-  getRead();
+  return false;
 });
 
 // Click event to mark a book as not read
@@ -48,29 +49,11 @@ $(document).on("click", ".markunread", function() {
 // Functions
 
 // Load unread books and render them to the screen
-function getUnread() {
-  $("#unread").empty();
-  $.getJSON("/unread", function(data) {
-    for (var i = 0; i < data.length; i++) {
-      $("#unread").prepend("<tr><td>" + data[i].title + "</td><td>" + data[i].author +
-        "</td><td><button class='markread' data-id='" + data[i]._id + "'>Mark Read</button></td></tr>");
-    }
-    $("#unread").prepend("<tr><th>Title</th><th>Author</th><th>Read/Unread</th></tr>");
+function getStatus() {
+  $.getJSON("/status", function(data) {
+    console.log(data)
   });
+  console.log('finished retrieving status')
 }
 
-// Load read books and render them to the screen
-function getRead() {
-  $("#read").empty();
-  $.getJSON("/read", function(data) {
-    for (var i = 0; i < data.length; i++) {
-      $("#read").prepend("<tr><td>" + data[i].title + "</td><td>" + data[i].author +
-        "</td><td><button class='markunread' data-id='" + data[i]._id + "'>Mark Unread</button></td></tr>");
-    }
-    $("#read").prepend("<tr><th>Title</th><th>Author</th><th>Read/Unread</th></tr>");
-  });
-}
-
-// Calling our functions
-getUnread();
-getRead();
+getStatus();
--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // Mongojs configuration
-var databaseUrl = "week18day3";
-var collections = ["books"];
+var databaseUrl = "foundersWeek";
+var collections = ["stream"];
 
 // Hook our mongojs config to the db var
 var db = mongojs(databaseUrl, collections);
@@ -37,30 +37,41 @@ db.on("error", function(error) {
 // Routes
 // ======
 
-// Post a book to the mongoose database
-app.post("/submit", function(req, res) {
+// Post a status to the mongoose database
+app.post("/update", function(req, res) {
 
-  // Save the request body as an object called book
-  var book = req.body;
+  // Save the request body as an object called status
+  var status = req.body;
+  console.log(status)
 
   // If we want the object to have a boolean value of false,
   // we have to do it here, because the ajax post will convert it
   // to a string instead of a boolean
-  book.read = false;
 
-  // Save the book object as an entry into the books collection in mongo
-  db.books.save(book, function(error, saved) {
+
+  // Save the status object as an entry into the books collection in mongo
+  db.stream.update(status, function(error, updated) {
     // Show any errors
     if (error) {
       console.log(error);
     }
     // Otherwise, send the response to the client (for AJAX success function)
     else {
-      res.send(saved);
+      res.send(updated);
+      console.log(updated)
     }
   });
 });
 
+app.get("/status", function(req, res) {
+	db.stream.find({ "youtube": true }, function(error, response) {
+		if (error) {
+			console.log(error);
+		} else {
+			res.json(response);
+		}
+	})
+})
 
 // Find all books marked as read
 app.get("/read", function(req, res) {
@@ -78,82 +89,8 @@ app.get("/read", function(req, res) {
 });
 
 
-// Find all books marked as unread
-app.get("/unread", function(req, res) {
-  // Go into the mongo collection, and find all docs where "read" is false
-  db.books.find({ "read": false }, function(error, found) {
-    // Show any errors
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the books we found to the browser as a json
-    else {
-      res.json(found);
-    }
-  });
-});
 
 
-// Mark a book as having been read
-app.get("/markread/:id", function(req, res) {
-  // Remember: when searching by an id, the id needs to be passed in
-  // as (mongojs.ObjectId(IDYOUWANTTOFIND))
-
-  // Update a doc in the books collection with an ObjectId matching
-  // the id parameter in the url
-  db.books.update({
-    "_id": mongojs.ObjectId(req.params.id)
-  }, {
-    // Set "read" to true for the book we specified
-    $set: {
-      "read": true
-    }
-  },
-  // When that's done, run this function
-  function(error, edited) {
-    // show any errors
-    if (error) {
-      console.log(error);
-      res.send(error);
-    }
-    // Otherwise, send the result of our update to the browser
-    else {
-      console.log(edited);
-      res.send(edited);
-    }
-  });
-});
-
-
-// Mark a book as having been not read
-app.get("/markunread/:id", function(req, res) {
-  // Remember: when searching by an id, the id needs to be passed in
-  // as (mongojs.ObjectId(IDYOUWANTTOFIND))
-
-  // Update a doc in the books collection with an ObjectId matching
-  // the id parameter in the url
-  db.books.update({
-    "_id": mongojs.ObjectId(req.params.id)
-  }, {
-    // Set "read" to false for the book we specified
-    $set: {
-      "read": false
-    }
-  },
-  // When that's done, run this function
-  function(error, edited) {
-    // Show any errors
-    if (error) {
-      console.log(error);
-      res.send(error);
-    }
-    // Otherwise, send the result of our update to the browser
-    else {
-      console.log(edited);
-      res.send(edited);
-    }
-  });
-});
 
 
 // Listen on port 3000
